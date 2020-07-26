d7153839794d9d6d5b6d6e83d05c21ac57852f03
Courtney Angotti <courtneyangotti@gmail.com>
2020-07-26 12:50:16 -0700

    Updates

---
+++ precache-manifest.af0913221e688e81f3eac3169170bba0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc421bf02798b97e8592fb41cfe1e523",
+    "url": "/robot/index.html"
+  },
+  {
+    "revision": "d1b9ec7db23b3b097d74",
+    "url": "/robot/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "528c48c5fd80d2800979",
+    "url": "/robot/static/css/main.b6939c0b.chunk.css"
+  },
+  {
+    "revision": "d1b9ec7db23b3b097d74",
+    "url": "/robot/static/js/2.a7e53781.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robot/static/js/2.a7e53781.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "528c48c5fd80d2800979",
+    "url": "/robot/static/js/main.9156fa0a.chunk.js"
+  },
+  {
+    "revision": "c2ea2ace4bd0cd957072",
+    "url": "/robot/static/js/runtime-main.e9015684.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robot/static/media/SEGA.12a3db8b.woff"
+  }
+]);
